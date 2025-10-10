afc5d0e3850690e4fe594e3cba098d1fdbf45763
Arthur ALLONCLE <arthuralloncle@gmail.com>
2025-10-10 09:48:18 +0200

    setup hero ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,65 +1,21 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
 
-interface IData {
-  status: Number
-  data: any
-}
+import IndexPage from "@/pages/index";
+import DocsPage from "@/pages/docs";
+import PricingPage from "@/pages/pricing";
+import BlogPage from "@/pages/blog";
+import AboutPage from "@/pages/about";
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [data, setData] = useState<IData>({status: 0, data: {}})
-  useEffect(() => {
-    const controller = new AbortController();
-    const getDefault = async () => {
-      console.log('fetch /');
-      try {
-        await axios.get(
-          'http://localhost/project/1'
-        ).then(res => {
-          setData(res.data)
-          console.log(res);
-        })
-      } catch (err) {
-        console.error(err);
-      } finally {
-        //stop loader
-      }
-    }
-    getDefault();
-    return () => controller.abort();
-  }, [])
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React + Docker</h1>
-      {/* <h2>{data.data}</h2> */}
-      <h2>{data.data.title}</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Routes>
+      <Route element={<IndexPage />} path="/" />
+      <Route element={<DocsPage />} path="/docs" />
+      <Route element={<PricingPage />} path="/pricing" />
+      <Route element={<BlogPage />} path="/blog" />
+      <Route element={<AboutPage />} path="/about" />
+    </Routes>
+  );
 }
 
-export default App
-
+export default App;
